784f29ab8b3a6c600318b44eafd42d072a8f3ffa
Mbonea-Mjema <mjema86@gmail.com>
2019-10-25 12:34:32 +0200

    first commit

---
+++ homework_1.m
@@ -0,0 +1,17 @@
+%%%Homework 1 
+
+%open Image
+image_1=imread('./Images/Image_1.jpg');
+image_2=imread('./Images/Image_2.jpg');
+image_3=imread('./Images/Image_3.jpg');
+
+
+%% Question 1
+%convert to grayscale
+
+image1_gray=rgb2gray(image_1);
+imshow(image1_gray)
+
+%% Question 2
+
+
